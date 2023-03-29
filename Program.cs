57ad293d9881f8a8fe65b8d5063ffdf9920e66d9
Martin <martinspuchliak@gmail.com>
2023-03-29 09:48:46 +0200

    Initialization

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SWI
+{
+    public class Rectangle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+    public class Square
+    {
+        public int Sides { get; set; }
+    }
+    class LSP
+    {
+        static public void Main(String[] args)
+        {
+            var rect = new Rectangle{Height=4,Width=6};
+            var square = new Square{Sides=3};
+    
+            Console.WriteLine("Rectangle area: " + rect.Height * rect.Width);
+            Console.WriteLine("Square area: " + square.Sides * square.Sides);
+        }
+    }
+}
