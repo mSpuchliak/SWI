9503e7080b09de35d1fa2169fb600b3751304b83
Martin <martinspuchliak@gmail.com>
2023-03-29 09:51:43 +0200

    Implemented the fix for the demeter law

--- Program.cs
+++ Program.cs
@@ -2,14 +2,27 @@
 
 namespace SWI
 {
-    public class Rectangle
+    public abstract class Shape
+    {
+        public abstract int getArea();
+    }
+    public class Rectangle :Shape
     {
         public int Height { get; set; }
         public int Width { get; set; }
+        public override int getArea()
+        {
+            return Height * Width;
+        }
     }
-    public class Square
+    public class Square :Shape
     {
         public int Sides { get; set; }
+        public override int getArea()
+        {
+            return Sides * Sides;
+        }
+
     }
     class LSP
     {
@@ -18,8 +31,8 @@ namespace SWI
             var rect = new Rectangle{Height=4,Width=6};
             var square = new Square{Sides=3};
     
-            Console.WriteLine("Rectangle area: " + rect.Height * rect.Width);
-            Console.WriteLine("Square area: " + square.Sides * square.Sides);
+            Console.WriteLine("Rectangle area: " + rect.getArea());
+            Console.WriteLine("Square area: " + square.getArea());
         }
     }
 }
